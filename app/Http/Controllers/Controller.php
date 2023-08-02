b6245af8ef5a471b6762aae7dae289b48abc40aa
mubar3 <khusnul.mubar@yahoo.com>
2023-08-02 16:15:01 +0700

    reset all

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,44 +10,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    public function git_pull()  
-    {
-        // $command = "cd .. && $(which git) pull";
-        // $command = "cd .. && where git pull";
-        // $command = "cd .. && git pull";
-        // $command = "cd .. && git status";
-        $repoPath = '/var/www/html/laravel_predis';
-        $gitPath = '/usr/bin/git'; // Replace this with the correct path to your git executable
-
-        
-        // Enable error reporting for troubleshooting
-        error_reporting(E_ALL);
-        ini_set('display_errors', 1);
-
-        // $command = "cd $repoPath && $gitPath pull";
-        $command = "cd $repoPath && cd";
-        $output = array();
-        $returnValue = null;
-        
-        exec($command, $output, $returnValue);
-
-        if ($returnValue === 0) {
-            // Command executed successfully
-            foreach ($output as $line) {
-                echo $line . "<br>";
-            }
-        } else {
-            // Command execution failed
-            echo "Command failed with error code: " . $returnValue;
-        }
-
-        
-        // $output=shell_exec($command);
-        // if ($output !== null) {
-        //     echo "Current working directory: " . $output;
-        // } else {
-        //     echo "Command execution failed.";
-        // }
-    }
 }
