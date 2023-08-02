0a2882545bb456dbf26370de9ca490758917cbc1
mubar3 <khusnul.mubar@yahoo.com>
2023-08-02 15:55:20 +0700

    add git pull

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,25 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function git_pull()  
+    {
+        // $command = "cd .. && $(which git) pull";
+        // $command = "cd .. && where git pull";
+        $command = "cd .. && git pull";
+        $output = array();
+        $returnValue = null;
+        
+        exec($command, $output, $returnValue);
+        
+        if ($returnValue === 0) {
+            // Command executed successfully
+            foreach ($output as $line) {
+                echo $line . "<br>";
+            }
+        } else {
+            // Command execution failed
+            echo "Command failed with error code: " . $returnValue;
+        }
+    }
 }
