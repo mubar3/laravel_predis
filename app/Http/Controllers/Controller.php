573230f506dcdf7d4a57bf66283404e3fae8e5fc
mubar3 <khusnul.mubar@yahoo.com>
2023-08-03 14:03:21 +0700

    add

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
@@ -13,6 +15,23 @@ class Controller extends BaseController
 
     public function git_pull() 
     {
-        return exec('cd .. && git pull');
+        $command = "cd .. && git pull";
+
+        try {
+            // Create a new Process instance with the Git pull command
+            $process = new Process(explode(' ', $command));
+
+            // Run the command
+            $process->mustRun();
+
+            // If the command runs successfully, you can optionally get the output
+            // $output = $process->getOutput();
+
+            return 'true'; // Git pull was successful
+        } catch (ProcessFailedException $exception) {
+            // If the command fails, you can get the error output for debugging
+            // $errorOutput = $exception->getProcess()->getErrorOutput();
+            return 'false'; // Git pull failed
+        }
     }
 }
