adc04c8f9532f5722354d21b25550c90a45be8e4
mubar3 <khusnul.mubar@yahoo.com>
2023-08-03 13:43:52 +0700

    add git pull

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/send_koordinat', [Broadcast_controller::class, 'send_koordinat']);
+Route::get('/git_pull', [App\Http\Controllers\Controller::class, 'git_pull']);
