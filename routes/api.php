2580c9b2d8cabe97a875038d76374233617de427
mubar3 <khusnul.mubar@yahoo.com>
2023-08-24 13:58:25 +0700

    add webhook

--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/send_koordinat', [Broadcast_controller::class, 'send_koordinat']);
 Route::get('/git_pull', [App\Http\Controllers\Controller::class, 'git_pull']);
+Route::any('/auto-git-pull', '\MichelMelo\MMAutoGitPull\MMAutoGitPullController@pull');
